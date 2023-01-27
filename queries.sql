b2b9f4f7ce5922162cffce1e5324c570e98689f4
sami24120 <samibenyacoub7@gmail.com>
2023-01-27 23:47:43 +0100

    dml y queries

---
+++ queries.sql
@@ -0,0 +1,18 @@
+select * from bike where type_bike = 'urban';
+
+select * from custom where surname = 'Rodriguez';
+
+select * from bike where price between 100 and 400;
+
+select * from store where name_store = 'NANO bicycles' and stars = 5;
+
+
+select distinct type_bike from bike order by type_bike;
+
+
+select from custom where age between 25 and 35;
+
+select from contract where start_rent between 2021-01-01 and 2022-01-01;
+
+
+select from extra where type_extra = 'None';
