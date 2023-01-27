92acd481718bceafaea74abd53e1c8c43562179d
kevin-coaquira <91737963+kevin-coaquira@users.noreply.github.com>
2023-01-28 00:23:41 +0100

    Add files via upload

--- drop.sql
+++ drop.sql
@@ -4,13 +4,15 @@ drop table custom;
 /*Bike*/
 drop table bike;
 alter table bike
-drop 
-/*Falta alter table y drop foreign key------------*/
+drop foreign key(id_store);
 /*Store*/
 drop table store;
 /*Insurance*/
 drop table insurance;
 /*Contract*/
-drop table contract /*Aqui necesitamos alter table y drop foreign key*/
+drop table contracts;
+alter table contracts
+drop foreign key(id_custom),
+drop foreign key(id_insurance);
 /*Extra*/
 drop table extra;
